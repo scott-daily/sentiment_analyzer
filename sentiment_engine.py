49d14e478604bee51e85ab9d79d530d659ac963c
Scott Daily <scottjuggles@gmail.com>
2020-03-15 12:23:23 -0400

    add praw.ini

--- sentiment_engine.py
+++ sentiment_engine.py
@@ -7,6 +7,6 @@ tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncase
 model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
 nlp_stars_sentiment = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 
-score_dict = nlp_stars_sentiment("I loved that pizza.")
+score_dict = nlp_stars_sentiment("Smaller jar, no peanut butter all over my fingers and wrists. Fresher. Was buying larger jars to get better pricing, but so messy trying to get it out near middle to end. Plus can be weeks before I eat again. This is less messy and still cost effective. I prefer Skippy, but I like this as 2nd choice.")
 
 print(score_dict[0])
