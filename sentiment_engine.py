5b51d8a2b6f38c8a0f3d331fab0b66d4eb1b58e1
Scott Daily <scottjuggles@gmail.com>
2021-03-28 10:52:17 -0400

    updated AutoModel to bert-base-cased.

--- sentiment_engine.py
+++ sentiment_engine.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer
 import praw
 
 tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased")
 nlp_stars_sentiment = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 
 '''score_dict = nlp_stars_sentiment("Smaller jar, no peanut butter all over my fingers and wrists. Fresher. Was buying larger jars to get better pricing, but so messy trying to get it out near middle to end. Plus can be weeks before I eat again. This is less messy and still cost effective. I prefer Skippy, but I like this as 2nd choice.")'''
